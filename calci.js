897e43ca97ff1af7ffab3d98274666b7a99c0743
bindu8829 <51020527+bindu8829@users.noreply.github.com>
2019-05-25 16:00:06 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,4 @@
+var calculation = require('./Arithmetic.js');
+
+console.log("The Multiplication of 4 and 5 is %d",calculation.Multiply(4,5));
+console.log("The Sum of 25 and 32 is %d",calculation.Add(25,32));
---
+++ sum.js
@@ -0,0 +1,3 @@
+var sum = require('./Addition.js');
+
+console.log("The sum of 4 and 5 is %d",sum.addNumber(4,5));
